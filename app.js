3813097609a3956c375a57666969a5e20639293c
Angel Aldrete <angel.aldrete96@gmail.com>
2023-01-11 10:54:08 -0800

    Added healthcheck endpoint

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-require('./config/db')
+require("./config/db");
 
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
 
 // Setup
-const PORT = process.env.PORT || 3000
-const UI_URI = process.env.UI_URI || 'http://localhost:8080'
+const PORT = process.env.PORT || 3000;
+const UI_URI = process.env.UI_URI || "http://localhost:8080";
 
 // Middleware
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: UI_URI, credentials: true }))
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: UI_URI, credentials: true }));
 
-const userRoutes = require('./routes/users')
-const reservationRoutes = require('./routes/reservations')
+const userRoutes = require("./routes/users");
+const reservationRoutes = require("./routes/reservations");
 
-const Reservation = require('./models/Reservation')
-const authMiddleware = require('./middleware/authMiddleware')
+const Reservation = require("./models/Reservation");
+const authMiddleware = require("./middleware/authMiddleware");
 
 // Routes
-app.use('/api/users', userRoutes)
-app.use('/api/reservations', reservationRoutes)
-app.get('/api/confirmation', authMiddleware, async (req, res) => {
+app.get("/", (req, res) => res.send("Ok!"));
+app.use("/api/users", userRoutes);
+app.use("/api/reservations", reservationRoutes);
+app.get("/api/confirmation", authMiddleware, async (req, res) => {
   try {
     let confirmationNumber = Math.floor(Math.random() * 10000000);
 
-    const existingConfirmationNumber = await Reservation.findOne({ confirmationNumber })
+    const existingConfirmationNumber = await Reservation.findOne({
+      confirmationNumber,
+    });
 
     if (existingConfirmationNumber) {
       while (confirmationNumber == existingConfirmationNumber) {
@@ -40,41 +43,42 @@ app.get('/api/confirmation', authMiddleware, async (req, res) => {
       }
     }
 
-    res.send({ confirmationNumber })
-
+    res.send({ confirmationNumber });
   } catch (err) {
-    console.error(err.stack)
+    console.error(err.stack);
   }
-})
-app.get('/api/available_rooms/:dateOfArrival/:dateOfDeparture', authMiddleware, async (req, res) => {
-  try {
-    const dateOfArrival = req.params.dateOfArrival
-    const dateOfDeparture = req.params.dateOfDeparture
+});
+app.get(
+  "/api/available_rooms/:dateOfArrival/:dateOfDeparture",
+  authMiddleware,
+  async (req, res) => {
+    try {
+      const dateOfArrival = req.params.dateOfArrival;
+      const dateOfDeparture = req.params.dateOfDeparture;
 
+      // Find reservations that overlap with the dates of arrival and departure
+      const reservations = await Reservation.find({
+        $and: [
+          { dateOfArrival: { $lte: dateOfArrival } },
+          { dateOfDeparture: { $gte: dateOfDeparture } },
+        ],
+      });
 
-    // Find reservations that overlap with the dates of arrival and departure
-    const reservations = await Reservation.find({
-      $and: [
-        { dateOfArrival: { $lte: dateOfArrival } },
-        { dateOfDeparture: { $gte: dateOfDeparture } }
-      ]
-    })
+      const occupiedRooms = reservations.map((reservation) => {
+        return reservation.occupiedRooms;
+      });
 
-    const occupiedRooms = reservations.map(reservation => {
-      return reservation.occupiedRooms
-    })
+      console.log(occupiedRooms);
 
-    console.log(occupiedRooms)
-
-    res.send(occupiedRooms)
-
-  } catch (err) {
-    console.error(err.stack)
+      res.send(occupiedRooms);
+    } catch (err) {
+      console.error(err.stack);
+    }
   }
-})
+);
 
 // Health Check
-app.get('/', (req, res) => res.sendStatus(200))
+app.get("/", (req, res) => res.sendStatus(200));
 
 // Entry point
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
